ddad3810bb46ff69855389fdc7a0cade6cc7afb5
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-06 21:05:33 +0530

    http methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  res.send("Hello from the server");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shubham", lastName: "Bhatt" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully send to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deletes successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("data patched successfully");
 });
 
 app.listen(3000, () => {
